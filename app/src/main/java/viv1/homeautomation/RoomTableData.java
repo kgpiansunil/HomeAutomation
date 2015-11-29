6d5a35e2ae666f9981d90237cefc0d5e59f4a1f7
Vivek Sahu <f2012058@pilani.bits-pilani.ac.in>
2015-11-29 15:11:46 +0530

    Class def for object to be stored in database

---
+++ app/src/main/java/viv1/homeautomation/RoomTableData.java
@@ -0,0 +1,45 @@
+package viv1.homeautomation;
+
+public class RoomTableData{
+
+    private String bid;
+    private String room_name;
+    private String room_address;
+    private String isVisible;
+
+    public String getRoom_address() {
+        return room_address;
+    }
+
+    public void setRoom_address(String room_address) {
+        this.room_address = room_address;
+    }
+
+    public String getBid() {
+        return bid;
+    }
+
+    public void setBid(String bid) {
+        this.bid = bid;
+    }
+
+    public String getRoom_name() {
+        return room_name;
+    }
+
+    public void setRoom_name(String room_name) {
+        this.room_name = room_name;
+    }
+
+    public String isVisible() {
+        return isVisible;
+    }
+
+    public void setVisible(String isVisible) {
+        this.isVisible = isVisible;
+    }
+
+
+
+
+}
